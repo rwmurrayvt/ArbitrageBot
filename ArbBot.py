8281f45debf55d584530d294c32bb6e372dc4eba
rwmurrayvt <rwmurray@vt.edu>
2014-05-01 14:18:36 -0400

    Update ArbBot.py

--- ArbBot.py
+++ ArbBot.py
@@ -14,10 +14,19 @@ while x == 1:
     nmcbtc = api.order_book('NMC/BTC/?depth=1')
     ghsnmc = api.order_book('GHS/NMC/?depth=1')
     r = re.compile('\d+.\d+')
-    ghsbtcdata = r.findall(ghsbtc)
-    nmcbtcdata = r.findall(nmcbtc)
-    ghsnmcdata = r.findall(ghsnmc)
-    print ghsbtcdata
-    print nmcbtcdata
-    print ghsnmcdata
+    GBdata = r.findall(ghsbtc)
+    NBdata = r.findall(nmcbtc)
+    GNdata = r.findall(ghsnmc)
+    GHSbuy = float(GNdata[4])
+    NMCneed = float(GNdata[4])*float(GNdata[3])
+    NMCavailable = float(NBdata[4])
+    if NMCavailable<NMCneed:
+        print 'BAD TRADE'
+    BTCneed = NMCneed*float(NBdata[3])
+    BTCavailable = float(GBdata[2])
+    if BTCavailable>BTCneed:
+        print 'BAD TRADE 2'
+    GHSsell = BTCneed/float(GBdata[1])
+    print GHSsell-GHSbuy
+#This while identify possible profit. Use place_order API to place orders.
     time.sleep(5)
